14e55cd543ceac44c596a92f4c96339ad42d3661
ls194@uni-heidelberg.de <hd_ls194@uc1n996.localdomain>
2019-02-01 12:04:32 +0100

    added MG file for 2 displaced leptons production

---
+++ MG5_aMC_v2_6_4/Singlet-triplet_MG_run_files/0_my_proc_generation_combined.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#The first script to run: creates the process folders
+#No need to run afterwards
+
+# set the paths
+MGdir=~/mywork/llp/soft_LLP/MG5_aMC_v2_6_4
+CARDdir=$MGdir/Singlet-triplet_MG_run_files
+
+# set proc card file
+PROC_CARD_Sc="proc_card_scalar.dat"
+PROC_CARD_Ps="proc_card_pseudo.dat"
+
+
+# go to MG and generate the process
+cd $MGdir
+./bin/mg5 $CARDdir/$PROC_CARD_Sc
+./bin/mg5 $CARDdir/$PROC_CARD_Ps
